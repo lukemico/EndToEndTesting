467abb78d207e89282bab72f843a9f91b851a809
luke <luke@bitscan.co>
2019-05-28 12:25:37 +1000

    Work with Document Cookies in Tests with Puppeteer

--- src/App.test.js
+++ src/App.test.js
@@ -1,82 +1,106 @@
 const puppeteer = require('puppeteer')
 const faker = require('faker')
 const devices = require('puppeteer/DeviceDescriptors')
-const iPhone = devices['iPhone 6'];
+const iPhone = devices['iPhone 6']
 
 const user = {
-    email: faker.internet.email(),
-    password: 'test',
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName()
+  email: faker.internet.email(),
+  password: 'test',
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName()
 }
-
-
 
 const isDebugging = () => {
   let debugging_mode = {
     headless: false,
     slowMo: 50,
-    devtools: true  
+    devtools: true
   }
-  return process.env.NODE_ENV === 'debug' ? debugging_mode : {};
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
 }
 
 let browser
 let page
+let logs = []
+let errors = []
 beforeAll(async () => {
   browser = await puppeteer.launch(isDebugging())
   page = await browser.newPage()
+
+  page.on('console', msg => logs.push(msg.text))
+  page.on('pageerror', error => errors.push(error.text))
+
   await page.goto('http://localhost:3000/')
-  page.setViewport({ width: 500, height: 2400 })
+  await page.emulate(iPhone)
 })
 
 describe('on page load ', () => {
-    test('h1 loads correctly', async () => {
-        const html = await page.$eval('[data-testid="h1"]', e => e.innerHTML)
+  test('h1 loads correctly', async () => {
+      const html = await page.$eval('[data-testid="h1"]', e => e.innerHTML)
 
-        expect(html).toBe('Welcome to React')
+      expect(html).toBe('Welcome to React')
 
     }, 16000)
 
-    test('nav loads correctly', async () => {
-        const navbar = await page.$eval('[data-testid="navbar"]', el => el ? true : false)
-        const listItems = await page.$$('[data-testid="navBarLi"]')
+  test('nav loads correctly', async () => {
+    const navbar = await page.$eval('[data-testid="navbar"]',
+      el => (el ? true : false)
+    )
+    const listItems = await page.$$('[data-testid="navBarLi"]')
 
-        expect(navbar).toBe(true)
-        expect(listItems.length).toBe(4)
+    expect(navbar).toBe(true)
+    expect(listItems.length).toBe(4)
+  })
+
+  describe('login form ', () => {
+    test('fills out form and submits', async () => {
+
+        await page.setCookie({ name: 'JWT', value: 'dsfksjdflksjdflskjdfkdkf' })
+
+        const firstNameEl = await page.$('[data-testid="firstName"]')
+        const lastNameEl = await page.$('[data-testid="lastName"]')
+        const emaildEl = await page.$('[data-testid="email"]')
+        const passwordEl = await page.$('[data-testid="password"]')
+        const submitEl = await page.$('[data-testid="submit"]')
+
+        await firstNameEl.tap()
+        await page.type('[data-testid="firstName"]', user.firstName)
+
+        await lastNameEl.tap()
+        await page.type('[data-testid="lastName"]', user.lastName)
+
+        await emaildEl.tap()
+        await page.type('[data-testid="email"]', user.email)
+
+        await passwordEl.tap()
+        await page.type('[data-testid="password"]', user.password)
+
+        await submitEl.tap()
+
+        await page.waitForSelector('[data-testid="success"]')
+    }, 16000)
+    
+    test('sets firstName cookie', async () => {
+      const cookies = await page.cookies()
+      const userCookie = cookies.find(x => x.name === 'firstName' && x.value === user.firstName)
+
+      expect(userCookie).not.toBeUndefined()
     })
+  })
 
-    test('login form works correctly', async () => {
-        const page2 = await browser.newPage()
-        await page2.emulate(iPhone)
-        await page2.goto('http://localhost:3000/')    
+//   test('does not have any console logs', () => {
+//     const newLogs = logs.filter( s => s !== '%cDownload the React DevTools for a better development experience: https://fb.me/react-devtools font-weight:bold')
+//     expect(newLogs.length).toBe(0)
+//   })
 
-        const firstName = await page2.$('[data-testid="firstName"]')
-        const lastName = await page2.$('[data-testid="lastName"]')
-        const email = await page2.$('[data-testid="password"]')
-        const password = await page2.$('[data-testid="password"]')
-        const submit = await page2.$('[data-testid="submit"]')
+  test('does not have exceptions', () => {
+    expect(errors.length).toBe(0)
+  })
 
-        await firstName.tap()
-        await page2.type('[data-testid="firstName"]', user.firstName)
-
-        await lastName.tap()
-        await page2.type('[data-testid="lastName"]', user.lastName)
-
-        await email.tap()
-        await page2.type('[data-testid="email"]', user.email)
-        
-        await password.tap()
-        await page2.type('[data-testid="password"]', user.password)
-
-        await submit.tap()
-
-        await page2.waitForSelector('[data-testid="success"]')
-    }, 16000)
 })
 
 afterAll(() => {
-    if (isDebugging()) {
-      browser.close()
-    }
-  })
+  if (isDebugging()) {
+    browser.close()
+  }
+})
